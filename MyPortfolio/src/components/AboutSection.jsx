055231388cf808c2b1253132804b6e1d4df3f45e
Jinhyeok <yanglucas96@gmail.com>
2026-01-15 16:15:19 +0900

    Update AboutMe Section into Korean

--- MyPortfolio/src/components/AboutSection.jsx
+++ MyPortfolio/src/components/AboutSection.jsx
@@ -4,7 +4,8 @@ function AboutSection() {
   return (
     <section>
       <h2 className="text-2xl font-bold mb-4">About Me</h2>
-      <p>Hi! I'm Jinhyeok, a passionate frontend and backend developer with experience in React, C#, PHP,and modern web design principles. I enjoy building accessible and visually engaging interfaces.</p>
+      <p>안녕하세요 저는 Jinhyeok입니다. React, C#, PHP 및 현대적인 웹 디자인 원칙을 사용한 프론트엔드 및 백엔드 개발자로서 열정을 가지고 있습니다. 
+        접근성과 시각적으로 매력적인 인터페이스를 구축하는 것을 좋아합니다.</p>
 
     </section>
   );
